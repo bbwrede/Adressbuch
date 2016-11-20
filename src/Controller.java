a8403022ea9249ef630e61fffc8273f60e452c0e
Fynn <Fynn@192.168.2.100>
2016-11-20 03:19:21 +0100

    1.0

---
+++ src/Controller.java
@@ -0,0 +1,231 @@
+public class Controller
+{
+	private List<Person> liste;
+	
+	Controller()
+	{
+		liste = new List<Person>();
+		test();
+	}
+	
+	
+	
+	void sortInNeu(Person neu)
+	{
+		liste.toFirst();
+		
+		if (liste.hasAccess() == false)
+		{
+			liste.insert(neu);
+		}
+		
+		OuterLoop:
+		while (liste.hasAccess())
+		{
+			int i = 0;
+			char x = neu.getNachname().charAt(i);
+			char y = liste.getContent().getNachname().charAt(i);
+			
+			if (Character.toLowerCase(y) > Character.toLowerCase(x))	
+			{
+				liste.insert(neu);
+				break OuterLoop;
+			}
+			
+			while (Character.toLowerCase(x) == Character.toLowerCase(y))
+			{
+				i++;
+				System.out.println(i);
+				
+				if (neu.getNachname().length() > i+1)
+				x = neu.getNachname().charAt(i);
+				else
+				{
+					liste.next();
+					liste.insert(neu);
+					break OuterLoop;
+				}
+					
+				if (liste.getContent().getNachname().length() > i+1)
+				y = liste.getContent().getNachname().charAt(i);
+				else
+				{
+					liste.next();
+					liste.insert(neu);
+					break OuterLoop;
+				}
+				
+				if (Character.toLowerCase(y) > Character.toLowerCase(x))	
+				{
+					liste.insert(neu);
+					break OuterLoop;
+				}
+			}
+			liste.next();
+		}
+	}
+	
+	
+	
+	
+	int sortIn(Person neu) // Gibt Index für JList zurück
+	{
+		liste.toFirst();
+		char a0 = neu.getNachname().charAt(0);
+		int index = 0;
+		
+		FirstCharLoop:
+		
+			
+		
+			
+		while (liste.hasAccess())
+		{
+			char b0 = liste.getContent().getNachname().charAt(0);
+			
+			
+			while (Character.toLowerCase(b0) == Character.toLowerCase(a0))
+			{
+				if (liste.getContent().getNachname().length() >= 2)
+				{
+				char a1 = neu.getNachname().charAt(1);
+				char b1 = liste.getContent().getNachname().charAt(1);
+				
+				while (Character.toLowerCase(b1) == Character.toLowerCase(a1))
+				{
+					if (liste.getContent().getNachname().length() >= 3)
+					{
+					char a2 = neu.getNachname().charAt(2);
+					char b2 = liste.getContent().getNachname().charAt(2);
+					
+					while (Character.toLowerCase(b2) == Character.toLowerCase(a2))
+					{
+						if (liste.getContent().getNachname().length() >= 4)
+						{
+						char a3 = neu.getNachname().charAt(3);
+						char b3 = liste.getContent().getNachname().charAt(3);
+						
+						while (Character.toLowerCase(b3) == Character.toLowerCase(a3))
+						{
+							if (liste.getContent().getNachname().length() >= 5)
+							{
+								char a4 = neu.getNachname().charAt(4);
+								char b4 = liste.getContent().getNachname().charAt(4);
+							
+								if (Character.toLowerCase(b4) > Character.toLowerCase(a4))
+								{
+									liste.insert(neu);
+									break FirstCharLoop;
+								}
+							
+								liste.next();
+								index++;
+							}
+							
+						}
+						
+						if (Character.toLowerCase(b3) > Character.toLowerCase(a3))
+						{
+							liste.insert(neu);
+							break FirstCharLoop;
+						}
+						
+						liste.next();
+						index++;
+					  }
+					}
+					
+					if (Character.toLowerCase(b2) > Character.toLowerCase(a2))
+					{
+						liste.insert(neu);
+						break FirstCharLoop;
+					}
+					
+					liste.next();
+					index++;
+				   }
+				}
+				
+				if (Character.toLowerCase(b1) > Character.toLowerCase(a1))
+				{
+					liste.insert(neu);
+					break FirstCharLoop;
+				}
+				
+				liste.next();
+				index++;
+				
+				}
+				
+			}
+			
+			if (Character.toLowerCase(b0) > Character.toLowerCase(a0))
+			{
+				liste.insert(neu);
+				break;
+			}
+			
+			liste.next();
+			index++;
+		}
+		
+		if (liste.hasAccess() == false)
+		{
+			liste.insert(neu);
+		}
+		
+		return index;
+	}
+	
+	void test()
+	{
+		Person p1 = new Person();
+		Person p2 = new Person();
+		Person p3 = new Person();
+		Person p4 = new Person();
+		Person p5 = new Person();
+		Person p6 = new Person();
+		Person p7 = new Person();
+		Person p8 = new Person();
+		p1.setNachname("Meier");
+		p2.setNachname("Lohse");
+		p3.setNachname("Behler");
+		p4.setNachname("Esders");
+		p5.setNachname("Knigge");
+		p6.setNachname("Lahsse");
+		
+		p7.setNachname("Lahsue");
+		p8.setNachname("Lohsee");
+		
+		sortInNeu(p1);
+		sortInNeu(p2);
+		sortInNeu(p3);
+		sortInNeu(p4);
+		sortInNeu(p5);
+		sortInNeu(p6);
+		System.out.println("Fertig");
+		sortInNeu(p7);
+		sortInNeu(p8);
+		ausgabe();
+	}
+	
+	void ausgabe()
+	{
+		liste.toFirst();
+		while (liste.hasAccess())
+		{
+			System.out.println(liste.getContent().getNachname());
+			liste.next();
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		Controller contrl = new Controller();
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/Person.java
@@ -0,0 +1,238 @@
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 12.09.2016
+  * @author 
+  */
+  
+import java.util.Date;
+
+public class Person {
+  
+  public enum Farbe
+  {
+    SCHWARZ, WEISS, BRAUN, GRUEN, BLAU, GRAU, ROT, GELB, LILA, ORANGE, PEACH, ABRICOT, MAGENTA, STERNENSTAUB, UNBEKANNT
+  }
+  
+  public enum Geschlecht
+  {
+    MAENNLICH, WEIBLICH, SONSTIGES, UNBEKANNT
+  }
+  
+  public enum Religion
+  {
+    MUSLIMISCH, KATHOLISCH, EVANGELISCH, JUEDISCH, ATHEISTISCH, UNBEKANNT
+  }
+  
+  // Anfang Attribute
+  private String vorname = "";
+  private String nachname = "";
+  private String land = "";
+  private String postleitzahl = "";
+  private String ort = "";
+  private String strasse = "";
+  private String hausnummer = "";
+  private String adresszusatz = "";
+  private String email = "";
+  private int groesse = 0;
+  private int gewicht = 0;
+  private Farbe augenfarbe = Farbe.UNBEKANNT;
+  private String telefon = "";
+  private String telefonMobil = "";
+  private String nationalitaet = "";
+  private Religion religion = Religion.UNBEKANNT;
+  private Farbe haarfarbe = Farbe.UNBEKANNT;
+  private Farbe hautfarbe = Farbe.UNBEKANNT;
+  private Geschlecht geschlecht = Geschlecht.UNBEKANNT;
+  // Ende Attribute
+  
+  // Anfang Methoden
+  public String getVorname() {
+    return vorname;
+  }
+
+  
+  public void setVorname(String vorname) {
+    this.vorname = vorname;
+  }
+
+  
+  public String getNachname() {
+    return nachname;
+  }
+
+  
+  public void setNachname(String nachname) {
+    this.nachname = nachname;
+  }
+
+  
+  
+  
+  public String getLand() {
+    return land;
+  }
+
+  
+  public void setLand(String land) {
+    this.land = land;
+  }
+
+  
+  public String getPostleitzahl() {
+    return postleitzahl;
+  }  
+  
+  public void setPostleitzahl(String postleitzahl) {
+    this.postleitzahl = postleitzahl;
+  }       
+  
+  public String getOrt() {
+    return ort;
+  }
+
+  
+  public void setOrt(String ort) {
+    this.ort = ort;
+  }
+
+  
+  public String getStrasse() {
+    return strasse;
+  }
+
+  
+  public void setStrasse(String strasse) {
+    this.strasse = strasse;
+  }
+
+  
+  public String getHausnummer() {
+    return hausnummer;
+  }
+
+  
+  public void setHausnummer(String hausnummer) {
+    this.hausnummer = hausnummer;
+  }
+
+  
+  public String getAdresszusatz() {
+    return adresszusatz;
+  }
+
+  
+  public void setAdresszusatz(String adresszusatz) {
+    this.adresszusatz = adresszusatz;
+  }
+
+  
+  public String getEmail() {
+    return email;
+  }
+
+  
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  
+  public int getGroesse() {
+    return groesse;
+  }
+
+  
+  public void setGroesse(int groesse) {
+    this.groesse = groesse;
+  }
+
+  
+  public int getGewicht() {
+    return gewicht;
+  }
+
+  
+  public void setGewicht(int gewicht) {
+    this.gewicht = gewicht;
+  }
+
+  
+  public Farbe getAugenfarbe() {
+    return augenfarbe;
+  }
+
+  
+  public void setAugenfarbe(Farbe augenfarbe) {
+    this.augenfarbe = augenfarbe;
+  }
+
+  
+  public String getTelefon() {
+    return telefon;
+  }
+
+  
+  public void setTelefon(String telefon) {
+    this.telefon = telefon;
+  }
+
+  
+  public String getTelefonMobil() {
+    return telefonMobil;
+  }
+
+  
+  public void setTelefonMobil(String telefonMobil) {
+    this.telefonMobil = telefonMobil;
+  }
+
+  
+  public String getNationalitaet() {
+    return nationalitaet;
+  }
+
+  
+  public void setNationalitaet(String nationalitaet) {
+    this.nationalitaet = nationalitaet;
+  }
+
+  
+  public Religion getReligion() {
+    return religion;
+  }
+
+  
+  public void setReligion(Religion religion) {
+    this.religion = religion;
+  }
+
+  
+  public Farbe getHaarfarbe() {
+    return haarfarbe;
+  }
+
+  
+  public void setHaarfarbe(Farbe haarfarbe) {
+    this.haarfarbe = haarfarbe;
+  }
+
+  
+  public Farbe getHautfarbe() {
+    return hautfarbe;
+  }
+
+  public void setHautfarbe(Farbe hautfarbe) {
+    this.hautfarbe = hautfarbe;
+  }
+
+  public Geschlecht getGeschlecht() {
+    return geschlecht;
+  }
+
+  public void setGeschlecht(Geschlecht geschlecht) {
+    this.geschlecht = geschlecht;
+  }
+
+  // Ende Methoden
+} // end of Person
